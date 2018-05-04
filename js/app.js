5fcc707d6179597ea411150fd06601041ef1f497
teefarie <toyinfari@gmail.com>
2018-05-04 12:58:10 +0100

    Text change

--- js/app.js
+++ js/app.js
@@ -17,20 +17,24 @@ $(document).ready(function() {
     $(".show-drop-down").click(function(e) {
         var drop_down_element = $(this).data('drop-down-id');
         if (drop_down_element !== null) {
-			$(".full-nav-holder").fadeOut('fast');
-			$(".show-drop-down").removeClass('active');
-
-            $(this).addClass('active');
-            $('#' + drop_down_element).fadeIn('fast');
+            $(".full-nav-holder").fadeOut('fast');
+            $(".show-drop-down").removeClass('active');
+            if (!$('#' + drop_down_element).is(':visible')) {
+                $(this).addClass('active');
+                $('#' + drop_down_element).fadeIn('fast');
+            } else {
+                $(this).removeClass('active');
+                $('#' + drop_down_element).fadeOut('fast');
+            }
         }
-		e.stopPropagation();
+        e.stopPropagation();
     });
 
     $(document).click(function(e) {
         var dropdown = $(".full-nav-holder");
         if (!dropdown.is(e.target) && dropdown.has(e.target).length === 0) {
             $(".full-nav-holder").fadeOut('fast');
-			$(".show-drop-down").removeClass('active');
+            $(".show-drop-down").removeClass('active');
         }
     });
 });
