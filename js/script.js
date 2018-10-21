4b4b8ddac2ec5c878297940248128dec3f2f067c
teefarie <toyinfari@gmail.com>
2018-10-21 13:18:02 +0100

    Updated version

--- js/script.js
+++ js/script.js
@@ -1,21 +1,11 @@
+//Preload
 $(window).on('load', function() { 
     $('#status').fadeOut();
     $('#preloader').delay(350).fadeOut('slow');
     $('body').delay(350).css({'overflow':'visible'});
 })
 
-
-/*$(window).scroll(function() {     
-    var scroll = $(window).scrollTop();
-    if (scroll > 0) {
-        $("nav").addClass("nav-border");
-    }
-    else {
-        $("nav").removeClass("nav-border");
-    }
-});*/
-
-
+//Back to top
 jQuery(document).ready(function($){
     var offset = 500,
         offset_opacity = 1200,
@@ -40,7 +30,7 @@ jQuery(document).ready(function($){
     });
 });
 
-
+//Animation fade in-up
 AOS.init({
   offset: 100,
   duration: 800,
@@ -50,17 +40,61 @@ AOS.init({
 });
 
 
-/*function checkOffset() {
+//Remove nav when footer is in view
+function checkOffset() {
     if($('nav').offset().top + $('nav').height() <= $('footer').offset().top - 0)
     $('nav').css('position', 'absolute');
     if($(document).scrollTop() + window.innerHeight < $('footer').offset().top)
     $('nav').css('position', 'fixed');
 }
+
 $(document).scroll(function() {
     checkOffset();
-});*/
+});
 
 
+//Make nav active on scroll to respective divs
+/*$(document).ready(function () {
+    $(document).on("scroll", onScroll);
+    
+    //smoothscroll
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+        
+        $('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+      
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top+2
+        }, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+});
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('.nav-container a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('.nav-container ul li a').removeClass("active");
+            currLink.addClass("active");
+        }
+        else{
+            currLink.removeClass("active");
+        }
+    });
+}*/
+
+//Animate to view when a link tag with an id is being clicked on
 $(document).on('click', 'a[href^="#"]', function (event) {
     event.preventDefault();
 
@@ -68,3 +102,27 @@ $(document).on('click', 'a[href^="#"]', function (event) {
         scrollTop: $($.attr(this, 'href')).offset().top
     }, 500);
 });
+
+
+//Hide div till nav is being clicked on
+jQuery(document).ready(function() {
+    jQuery('#case-studies').hide();
+
+    jQuery('.hide-div-c').click(function(e){
+        e.preventDefault();jQuery("#case-studies").slideToggle();
+        jQuery('.hide-div-c').toggleClass('opened closed');
+    });
+});
+
+jQuery(document).ready(function() {
+    jQuery('#get-in-touch').hide();
+
+    jQuery('.hide-div-g').click(function(e){
+        e.preventDefault();jQuery("#get-in-touch").slideToggle();
+        jQuery('.hide-div-g').toggleClass('opened closed');
+    });
+
+});
+
+
+
